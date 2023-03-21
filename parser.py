488acf09395eb1b7871e52d7c6ed20c7bbe5a830
Deserag <98634457+Deserag@users.noreply.github.com>
2023-03-21 20:35:43 +0300

    parser v.2
    2 версия парсера

--- parser.py
+++ parser.py
@@ -5,12 +5,78 @@ book = load_workbook(filename="C:/Users/Deserag/Downloads/Telegram Desktop/10%20
 # 65-90 A-Z
 
 sheet = book['Колледж ВятГУ']
-pattern = r"Группа .{1,2}к"
+pattern = r"Группа .{2,3}к"
 for i in range(65,91):
-     print( sheet[chr(i)+'24'].value)
-     if  sheet[chr(i)+'24'].value and re.match(pattern,sheet[chr(i)+'24'].value):
-        begin_value = chr(i)+'24'
-        print(sheet[chr(i)+'24'].value)
-        break
-print(begin_value)
+        val = sheet[chr(i)+'24'].value
+        if val and val[0]==" ":
+                val=val[1:len(val)]
+        if val and re.match(pattern,val):
+                begin_value = i
+                print(sheet[chr(i)+'24'].value)
+                break
+simvol = chr(begin_value)
+kolich = 0
+def change_sim(simvol):
+                if len(simvol)<2:
+                        if ord(simvol)<90:
+                                simvol = chr(ord(simvol)+1)
+                        else:
+                                simvol= "AA"
+                else:
+                        if ord(simvol[-1])<90:
+                                simvol = simvol[0:len(simvol)-1]+chr(ord(simvol[-1])+1)                                
+                        else:
+                                simvol = chr(ord(simvol[-2])+1)+"A"
+                return simvol
+pars =['1 пара', '2 пара', '3 пара', '4 пара', '5 пара', '6 пара', '7 пара']
+days =['понедельник', 'вторник', 'среда', 'четверг', 'пятгница', 'суббота', '7 пара']
+done_group = 0
+groups_with_data = []
+while True:
+        if kolich >= 10:
+               break
+        val = sheet[simvol+'24'].value
+        if val:
+                kolich=0
+                x = 26
+               
+                for i in range(40):
+                        groups_with_data.append([val,pars[i%7],sheet[simvol+str(x)].value])
+                        # print(sheet[simvol+str(x)].value)
+                        x+=1
+                x = 26
+                simvol=change_sim(simvol)
+                for i in range(40):
+                        groups_with_data[done_group].append(sheet[simvol+str(x)].value)
+                        # print(sheet[simvol+str(x)].value)
+                        done_group+=1
+                        x+=1
+                done_group -=40
+                x = 26
+                simvol=change_sim(simvol)
+                
+                
+                for i in range(40):
+                        groups_with_data[done_group].append(sheet[simvol+str(x)].value)
+                        # print(sheet[simvol+str(x)].value)
+                        done_group+=1
+                        x+=1
+                done_group -=40
+                x = 26
+                simvol=change_sim(simvol)
+                
+                for i in range(40):
+                        groups_with_data[done_group].append(sheet[simvol+str(x)].value)
+                        # print(sheet[simvol+str(x)].value)
+                        done_group+=1
+                        x+=1
+                simvol=change_sim(simvol)
+                
+        else: 
+                simvol=change_sim(simvol)
+                
+                kolich +=1
+
+for i in groups_with_data:
+        print(i)
 
