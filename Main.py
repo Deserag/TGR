64e849482e8614ce33cf33cff15f5f8c94b363f3
Deserag <98634457+Deserag@users.noreply.github.com>
2023-03-30 21:49:48 +0300

    Первый файл

--- Main.py
+++ Main.py
@@ -9,7 +9,7 @@ async def send_welcome(message: types.Message):
 @dp.message_handler() 
 async def echo(message: types.Message): 
     await message.answer(message.text)
-
+#
 
 if __name__ == '__main__':
    executor.start_polling(dp, skip_updates=True)
